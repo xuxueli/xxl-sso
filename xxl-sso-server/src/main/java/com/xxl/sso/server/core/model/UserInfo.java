de187aca594d5001e3f48e0f38ae041a7cee402c
xueli.xue <xueli.xue@dianping.com>
2018-04-04 19:26:59 +0800

    更新RM

--- xxl-sso-server/src/main/java/com/xxl/sso/server/core/model/UserInfo.java
+++ xxl-sso-server/src/main/java/com/xxl/sso/server/core/model/UserInfo.java
@@ -1,15 +1,36 @@
 package com.xxl.sso.server.core.model;
 
-import lombok.Data;
-
 /**
  * @author xuxueli 2018-03-22 23:51:51
  */
-@Data
 public class UserInfo {
 
     private int id;
     private String username;
     private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
 }
