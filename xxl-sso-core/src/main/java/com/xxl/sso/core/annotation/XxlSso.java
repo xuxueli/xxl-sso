c7f82db8cca2b2a75f38daafa9c3af6c1d2d97c9
xuxueli <931591021@qq.com>
2025-07-27 12:58:48 +0800

    update doc

--- xxl-sso-core/src/main/java/com/xxl/sso/core/annotation/XxlSso.java
+++ xxl-sso-core/src/main/java/com/xxl/sso/core/annotation/XxlSso.java
@@ -10,10 +10,10 @@ import java.lang.annotation.Target;
  * xxl-sso annotation
  *
  * <pre>
- * 		@Permission								: need login, but not valid permission or role
- * 		@Permission(login = false)				: not need login, not valid anything
- * 		@Permission(permission = "user:add")	: need login, and valid permission
- * 		@Permission(role = "admin")				: need login, and valid role
+ * 		@XxlSso								: need login, but not valid permission or role
+ * 		@XxlSso(login = false)				: not need login, not valid anything
+ * 		@XxlSso(permission = "user:add")	: need login, and valid permission
+ * 		@XxlSso(role = "admin")				: need login, and valid role
  * </pre>
  *
  * @author xuxueli 2015-12-12 18:29:02
